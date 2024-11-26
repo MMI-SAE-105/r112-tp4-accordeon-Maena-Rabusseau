7b112c3d6dca79eb5fe9f2b58e9ef8d9d7ef2707
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2024-11-26 15:26:54 +0100

    Ajout js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
 // *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+accordeon.addEventListener("click", () => {
+    accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
+});
