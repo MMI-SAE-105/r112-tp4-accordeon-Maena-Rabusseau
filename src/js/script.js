658dcdc447c7c5d334b1d48aa2c76291caef18b0
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2024-11-26 15:28:00 +0100

    referme les détails suite à un clic sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,4 @@ const accordeon = document.querySelector(".accordeon");
 accordeon.addEventListener("click", () => {
     accordeon.querySelectorAll("details").forEach((det) => (det.open = false));
 });
+;
